d46f0bf00499f25ece23f82e082d7d7a8b232aae
MathisDS15 <mathisds1506@gmail.com>
2025-09-19 10:56:15 +0100

    I added my first class 'BayesianModel' which allows us have a base for each statistical models.

--- src/models/models.py
+++ src/models/models.py
@@ -4,9 +4,26 @@ Class implementing the base of all models
 by Mathis DA SILVA
 """
 
+
+import pymc as pm
+
+
 class BayesianModel:
 
-    def __init__(self, name, fitted = False, parameters = None):
+    def __init__(self, name: str, model, trace):
         self.name = name
-        self.fitted = fitted
-        self.parameters = parameters
+        self.model = model
+        self.trace = trace
+
+    def build_model(self, datasets: dict):
+        pass
+
+    def fit(self, draws = 1000, chains = 4, target_accept = 0.8):
+        with self.model:
+            self.trace = pm.sample(
+                draws = draws,
+                chains = chains,
+                target_accept = target_accept,
+                return_inferencedata = True
+            )
+        return self.trace
